b64bdf257b941f41f4c94c5d36536499fb73934b
Skoobary <83435558+Skoobary@users.noreply.github.com>
2021-06-28 17:55:08 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -8,31 +8,37 @@ remoteServer = input("Enter a remote host's Ipv4 address to be scan: ")
 remoteServerIP  = socket.gethostbyname(remoteServer)
 ports_amt = int(input("How many ports shall be checked ? : ")) + 2
 
-print ("--------")
-print ("Please wait, scanning remote host"), remoteServerIP
-print ("--------")
+def main():
+    
+    print ("--------")
+    print ("Please wait, scanning remote host"), remoteServerIP
+    print ("--------")
 
-# Check what time the scan started
-t1 = datetime.now()
 
-try:
-    for port in range(1, ports_amt):  
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        result = sock.connect_ex((remoteServerIP, port))
-        if result == 0:
-            print("Port {}:    [Open]".format(port))
-        sock.close()
+    # Check what time the scan started
+    t1 = datetime.now()
 
-except socket.gaierror:
-    print ('Hostname could not be resolved...')
-    sys.exit()
+    try:
+        for port in range(1, ports_amt):  
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            result = sock.connect_ex((remoteServerIP, port))
+            if result == 0:
+                print("Port {}:    [Open]".format(port))
+            sock.close()
 
-except socket.error:
-    print ("[CONNEXION ERROR] Couldn't connect to server")
-    sys.exit()
+    except socket.gaierror:
+        print ('Hostname could not be resolved...')
+        sys.exit()
 
-# Checking the time again
-t2 = datetime.now()
-total =  t2 - t1
+    except socket.error:
+        print ("[CONNEXION ERROR] Couldn't connect to server")
+        sys.exit()
 
-print('Scanning Completed in: ' + total)
+    # Checking the time again
+    t2 = datetime.now()
+    total =  t2 - t1
+
+    print('Scanning Completed in: ' + total)
+
+if __name__ == "__main__":
+    main()
